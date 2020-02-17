6856834811b83fe818dda9fcbbd6a244b763fe46
Ben Coleman <colemanb@moravian.edu>
2020-02-16 21:10:19 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyjen",
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
+
---
+++ tests/test_foo.py
@@ -0,0 +1,6 @@
+from pyjen.foo import bar
+
+def test_bar():
+	assert bar() == 42
+
+
